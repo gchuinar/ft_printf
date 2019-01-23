522c615c7dc9e10788a8c6ae35b4dd54883e02a8
Gautier CHUINARD <gchuinar@student.42.fr>
2019-01-23 05:28:52 +0100

    probleme sur la retenue de la multiplication

--- src/ft_do_mult.c
+++ src/ft_do_mult.c
@@ -6,17 +6,51 @@
 /*   By: gchuinar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 20:10:55 by gchuinar          #+#    #+#             */
-/*   Updated: 2019/01/23 01:18:43 by gchuinar         ###   ########.fr       */
+/*   Updated: 2019/01/23 05:27:43 by gchuinar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/printf.h"
+#include <stdio.h>
 
 static int	ft_ret(int nbr)
 {
 	if (nbr > 9)
 		return (nbr / 10);
 	return (0);
+}
+
+static char	*ft_swap(char *str)
+{
+	int	i;
+
+	i = 0;
+	printf("str swap = %s\n", str);
+	while (str[i] < '0' || str[i] > '9')
+		i++;
+	while(str[i] != '\0')
+	{
+		str[i - 1] = str[i];
+		i++;
+	}
+	printf("str swap apres while = %s\n", str);
+	str[i - 1] = '0';
+	printf("str swap avant return = %s\n", str);
+	return (str);
+}
+
+static char	*ft_add_0_diz(char *sub_result, int len, int j)
+{
+	int	i;
+
+	i = len - (j + 1);
+	len = ft_strlen(sub_result) - 1;
+	while (i > 0)
+	{
+		sub_result = ft_swap(sub_result);
+		i--;
+	}
+	return (sub_result);
 }
 
 static char	*ft_decal(char *str)
@@ -43,33 +77,44 @@ static char	*ft_mult(char *nb1, char *nb2)
 
 	len = (int)(ft_strlen(nb1) + ft_strlen(nb2));
 	result = ft_strnew(len);
-	result = ft_fill_zero(result, len);
+	ft_fill_zero(result, len);
 	j = ft_strlen(nb2) - 1;
 	ret = 0;
+	sub_result = ft_strnew(len);
 	while (len >= 0 && j >= 0)
 	{
-		sub_result = ft_strnew(len);
-		sub_result = ft_fill_A(sub_result, len);
+		ft_fill_A(sub_result, len);
+		printf("len = %d\n", len);
+		printf("j = %d\n", j);
 		i = ft_strlen(nb1) - 1;
 		while (i >= 0)
 		{
+			printf("i = %d\n", i);
 			nbr = ((nb1[i] - '0') * (nb2[j] - '0') + ret);
-			sub_result[len] = (nbr % 10) + '0';
-			if((ret = ft_ret(nbr) == 0))
+			printf("nbr = %d\n", nbr);
+			printf("nb1 - '0' = %d\n", nb1[i] - '0');
+			printf("nb2 - '0' = %d\n", nb2[j] - '0');
+			printf("ret = %d\n", ret);
+			if(((ret = ft_ret(nbr)) == 0))
 				sub_result[len] = (nbr) + '0';
-			else if ((ret = ft_ret(nbr) == 0))
-				sub_result[len] = nbr + '0';
+			else if (((ret = ft_ret(nbr)) == 0))
+				sub_result[len] = (nbr % 10) + '0';
 			i--;
+			printf("sub_result dans le while = %s\n", sub_result);
+			printf("ret fin = %d\n", ret);
+			len--;
 		}
-		sub_result = ft_decal(sub_result);
+		len = (int)(ft_strlen(nb1) + ft_strlen(nb2));
+		printf("sub_result = %s\n", sub_result);
+		sub_result = ft_decal(ft_add_0_diz(sub_result, ft_strlen(nb2), j));
 		j--;
-		len--;
 		result = ft_doop_str(result, '+', sub_result);
+		printf("result fin de boucle = %s\n", result);
 	}
 	return (result);
 }
 
-char	*ft_do_mult(nb1, nb2, result)
+char	*ft_do_mult(char *nb1, char *nb2, char *result)
 {
 	int	i;
 	int	j;
@@ -78,14 +123,17 @@ char	*ft_do_mult(nb1, nb2, result)
 	j = 0;
 	if (ft_sizecmp(nb1, nb2) == 1)
 	{
+		printf("size = 1\n");
 		result = ft_mult(nb1, nb2);
 	}
 	else if (ft_sizecmp(nb1, nb2) == 2)
 	{
+		printf("size = 2\n");
 		result = ft_mult(nb2, nb1);
 	}
 	else if (ft_sizecmp(nb1, nb2) == 0)
 	{
+		printf("size = 0\n");
 		result = ft_mult(nb1, nb2);
 	}
 
@@ -94,11 +142,12 @@ char	*ft_do_mult(nb1, nb2, result)
 
 int	main(void)
 {
-	const char *a;
-	const char *b;
+	char *a;
+	char *b;
 
-	a = (const char*)ft_strdup("321");
-	b = (const char*)ft_strdup("10");
+	a = ft_strdup("54321");
+	b = ft_strdup("12");
 	ft_putendl(ft_doop_str(a, '*', b));
+	ft_putendl("probleme de retenu");
 	return (0);
 }
